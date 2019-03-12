c2fb54d74ecf6a7ec75f050bcc8e90efc87e6221
jdbbaugh <jbaugh78@msn.com>
2019-03-12 11:47:16 -0500

    set up file structure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace studendExercises
@@ -28,6 +29,26 @@ namespace studendExercises
                 Name = "Make a Dictionary",
                 CodeLanguage = ".NET"
             };
+            Exercise overlyExcited = new Exercise()
+            {
+                Name = "Make a database that manipulates time.",
+                CodeLanguage = ".NET"
+            };
+            Exercise solarSystem = new Exercise()
+            {
+                Name = "Learn about planets.",
+                CodeLanguage = "C#"
+            };
+            Exercise carLot = new Exercise()
+            {
+                Name = "Manipulate carlot database.",
+                CodeLanguage = "Javascript"
+            };
+            Exercise dynamicCards = new Exercise()
+            {
+                Name = "Play uno and goFish.",
+                CodeLanguage = "React"
+            };
             // Create 3, or more, cohorts.
             Cohort cohort28 = new Cohort("Cohort 28");
             Cohort cohort29 = new Cohort("Cohort 29");
@@ -42,6 +63,14 @@ namespace studendExercises
             cohort29.Students.Add(student3);
             Student student4 = new Student("Jill", "Schmill", "slack/herns", cohort30);
             cohort30.Students.Add(student4);
+            Student larry = new Student("Larry", "Berry", "slack/larBer", cohort28);
+            cohort30.Students.Add(larry);
+            Student kristin = new Student("Kristin", "Alberts", "slack/albie", cohort29);
+            cohort30.Students.Add(kristin);
+            Student loshana = new Student("Loshana", "Obanson", "slack/obslo", cohort29);
+            cohort30.Students.Add(loshana);
+            Student tre = new Student("Tre", "Strother", "slack/obslo", cohort30);
+            cohort30.Students.Add(tre);
 
             // Create 3, or more, instructors and assign them to one of the cohorts.
             Instructor Jisie = new Instructor("Jisie", "David", "slack/jisieD", cohort30);
@@ -62,14 +91,43 @@ namespace studendExercises
             List<Student> students = new List<Student>(){student1, student2,student3,student4};
             List<Exercise> exercises = new List<Exercise>(){learnJavaScript, learnReact, learnCSharp, learnDotNet};
 
-            foreach(Student student in students){
-                List<string> studentIsWorkingOn = new List<string>();
-                foreach(Exercise exercise in student.CurrentExercises){
-                    studentIsWorkingOn.Add(exercise.Name);
-                }
-                Console.WriteLine();
-                Console.WriteLine($"{student.FirstName} {student.LastName} is working on Assignments: {String.Join(", ", studentIsWorkingOn)}");
-            }
+            // foreach(Student student in students){
+            //     List<string> studentIsWorkingOn = new List<string>();
+            //     foreach(Exercise exercise in student.CurrentExercises){
+            //         studentIsWorkingOn.Add(exercise.Name);
+            //     }
+            //     Console.WriteLine();
+            //     Console.WriteLine($"{student.FirstName} {student.LastName} is working on Assignments: {String.Join(", ", studentIsWorkingOn)}");
+            // }
+
+            List<Student> moreStudents = new List<Student>(){
+                larry,
+                kristin,
+                loshana,
+                tre
+            };
+            List<Exercise> moreExercises = new List<Exercise>() {
+                overlyExcited,
+                solarSystem,
+                carLot,
+                dynamicCards
+            };
+            List<Cohort> moreCohorts = new List<Cohort>(){
+                cohort28,
+                cohort29,
+                cohort30
+            };
+            List<Instructor> moreInstructors = new List<Instructor>(){
+                Jisie,
+                Andy,
+                Leah
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine(moreExercises.Where(ex => ex.CodeLanguage == "Javascript"));
+
         }
     }
 }
