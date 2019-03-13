0b4cb52740768f18755bc7e89b1202502a78bd78
jdbbaugh <jbaugh78@msn.com>
2019-03-12 19:30:51 -0500

    few more problems to go

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 namespace studendExercises
 {
+    public class CohortQuery {
+        public string CohortName {get; set;}
+        public List<string> Students {get; set;}
+    }
     class Program
     {
         static void Main(string[] args)
@@ -11,8 +15,8 @@ namespace studendExercises
             // Create 4, or more, exercises.
             Exercise learnJavaScript = new Exercise()
             {
-                Name = "Make a user input",
-                CodeLanguage = "JavaScript"
+                Name = "LearnJavaScript",
+                CodeLanguage = "Javascript"
             };
             Exercise learnReact = new Exercise()
             {
@@ -26,8 +30,8 @@ namespace studendExercises
             };
             Exercise learnDotNet = new Exercise()
             {
-                Name = "Make a Dictionary",
-                CodeLanguage = ".NET"
+                Name = "Dictionary",
+                CodeLanguage = "Javascript"
             };
             Exercise overlyExcited = new Exercise()
             {
@@ -41,7 +45,7 @@ namespace studendExercises
             };
             Exercise carLot = new Exercise()
             {
-                Name = "Manipulate carlot database.",
+                Name = "Carlot.",
                 CodeLanguage = "Javascript"
             };
             Exercise dynamicCards = new Exercise()
@@ -53,6 +57,7 @@ namespace studendExercises
             Cohort cohort28 = new Cohort("Cohort 28");
             Cohort cohort29 = new Cohort("Cohort 29");
             Cohort cohort30 = new Cohort("Cohort 30");
+            Cohort cohort31 = new Cohort("Cohort 31");
 
             // Create 4, or more, students and assign them to one of the cohorts.
             Student student1 = new Student("Jimbo", "Gimbo", "slack/jmbo", cohort28);
@@ -69,15 +74,21 @@ namespace studendExercises
             cohort30.Students.Add(kristin);
             Student loshana = new Student("Loshana", "Obanson", "slack/obslo", cohort29);
             cohort30.Students.Add(loshana);
-            Student tre = new Student("Tre", "Strother", "slack/obslo", cohort30);
+            Student tre = new Student("Tre", "Strother", "slack/obslo", cohort31);
             cohort30.Students.Add(tre);
+            Student bob = new Student("Bob", "Hombre", "slack/obslo", cohort31);
+            cohort30.Students.Add(bob);
 
             // Create 3, or more, instructors and assign them to one of the cohorts.
             Instructor Jisie = new Instructor("Jisie", "David", "slack/jisieD", cohort30);
             cohort30.Instructors.Add(Jisie);
             Instructor Andy = new Instructor("Andy", "Collins", "slack/andyC", cohort29);
             cohort30.Instructors.Add(Andy);
-            Instructor Leah = new Instructor("Leah", "Hollen", "slack/andyC", cohort28);
+            Instructor Maddi = new Instructor("Maddi", "Albs", "slack/MaddiC", cohort29);
+            cohort30.Instructors.Add(Maddi);
+            Instructor Leah = new Instructor("Leah", "Hollen", "slack/leah", cohort28);
+            cohort30.Instructors.Add(Leah);
+            Instructor Joe = new Instructor("Joe", "Shepherd", "slack/jo", cohort31);
             cohort30.Instructors.Add(Leah);
 
             // Have each instructor assign 2 exercises to each of the students.
@@ -106,12 +117,16 @@ namespace studendExercises
                 loshana,
                 tre
             };
+            moreStudents.AddRange(students);
+
             List<Exercise> moreExercises = new List<Exercise>() {
                 overlyExcited,
                 solarSystem,
                 carLot,
                 dynamicCards
             };
+
+            moreExercises.AddRange(exercises);
             List<Cohort> moreCohorts = new List<Cohort>(){
                 cohort28,
                 cohort29,
@@ -120,13 +135,84 @@ namespace studendExercises
             List<Instructor> moreInstructors = new List<Instructor>(){
                 Jisie,
                 Andy,
-                Leah
+                Leah,
+                Maddi
             };
 
             Console.WriteLine();
             Console.WriteLine("-----------------------------");
 
-            Console.WriteLine(moreExercises.Where(ex => ex.CodeLanguage == "Javascript"));
+
+        // List exercises for the JavaScript language by using the Where() LINQ method.
+
+            // List<Exercise> languageQuery = (from ex in moreExercises
+            // where ex.CodeLanguage == "Javascript"
+            // select ex).ToList();
+
+            var languageQuery = moreExercises.Where(exercise => exercise.CodeLanguage == "Javascript");
+
+            Console.WriteLine("The following exercises use Javascript");
+            foreach(Exercise assignment in languageQuery){
+                Console.WriteLine(assignment.Name);
+            }
+
+            // List students in a particular cohort by using the Where() LINQ method.
+            List<Student> studentsInCohort = moreStudents.Where(student => student.CohortNumber == cohort29).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine("Students in Cohort 29");
+            Console.WriteLine("------------------------");
+            foreach(Student student in studentsInCohort){
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+
+
+            // List instructors in a particular cohort by using the Where() LINQ method.
+            List<Instructor> instructorsInCohort = moreInstructors.Where(Instructor => Instructor.CohortNumber == cohort29).ToList();
+            Console.WriteLine();
+            Console.WriteLine("Instructor in Cohort 29");
+            Console.WriteLine("------------------------");
+            foreach(Instructor inst in instructorsInCohort){
+                Console.WriteLine($"{inst.FirstName} {inst.LastName}");
+            }
+
+
+
+            // Sort the students by their last name.
+            // List<Student> studentSort = (from student in moreStudents
+            //     orderby student.LastName
+            //     select student).ToList();
+
+
+            var studentSort = moreStudents.OrderBy(stu => stu.LastName);
+
+            Console.WriteLine();
+            Console.WriteLine("Students alphabetically");
+            Console.WriteLine("-----------------------------------");
+                foreach(Student student in studentSort) {
+                    Console.WriteLine($"{student.LastName} {student.FirstName}");
+                }
+
+            //TODO: Display any students that aren't working on any
+            // exercises (Make sure one of your student instances don't have
+            // any exercises. Create a new student if you need to.)
+            List<Student> noWork = moreStudents.Where(student => student.CurrentExercises[0] == "").ToList();
+                        Console.WriteLine();
+            Console.WriteLine("Students with no current exercises to work on");
+            Console.WriteLine("-----------------------------------");
+            foreach(Student student in noWork) {
+                Console.WriteLine(student.FirstName);
+            }
+
+
+
+
+            // TODO:Which student is working on the most exercises? Make sure one of your students has more exercises than the others.
+
+            // TODO: How many students in each cohort?
+
+
+
 
         }
     }
